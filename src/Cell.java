457259a62849004fa643391fcbecdfdcaea1a3e7
Michael Long <michaeltlong10@gmail.com>
2022-09-15 15:01:16 -0400

    first commit

---
+++ src/Cell.java
@@ -0,0 +1,16 @@
+public class Cell {
+
+  private boolean isAlive;
+
+  public Cell() {
+    isAlive = false;
+  }
+
+  public boolean isAlive() {
+    return isAlive;
+  }
+
+  public void setAlive(boolean alive) {
+    isAlive = alive;
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main {
+  public static final int WIDTH = 800, HEIGHT = 800;
+
+  public static void main(String[] args) {
+
+    JFrame window = new JFrame("");
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setBounds(0, 0, WIDTH, HEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+    GameOfLifePanel panel = new GameOfLifePanel(WIDTH, HEIGHT);
+
+    panel.setFocusable(true);
+    panel.grabFocus();
+
+    window.add(panel);
+
+    window.setVisible(true);
+    window.setResizable(true);
+  }
+}
